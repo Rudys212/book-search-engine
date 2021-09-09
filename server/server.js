077fb924ac37a06671236d5717a6293c8d0641eb
Rudy Sanchez <rudys212@gmail.com>
2021-09-09 16:08:21 -0700

    localhost test

--- server/server.js
+++ server/server.js
@@ -4,7 +4,7 @@ const path = require("path");
 const db = require("./config/connection");
 const { typeDefs, resolvers } = require("./schemas");
 const { authMiddleware } = require("./utils/auth");
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 
 const server = new ApolloServer({
   typeDefs,
